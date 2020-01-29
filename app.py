f964df2809ba90e87eb83953f459e2e1c29b18f0
coolngr8 <16436680+coolngr8@users.noreply.github.com>
2020-01-29 19:11:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ Created on 04-Sep-2019
 """
 hello_flask: First Python-Flask webapp
 """
-from flask import Flask  # From module flask import class Flask
+from flask import Flask, request  # From module flask import class Flask
 app = Flask(__name__)    # Construct an instance of Flask class for our webapp
 
 @app.route('/', methods=['POST'])   # URL '/' to be handled by main() route handler
